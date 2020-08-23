3ad11129d96877fbadd4ddcd159b87024b21d42a
Maksim <43318993+DarkBullNull@users.noreply.github.com>
2020-08-24 01:24:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ConfuserDeobfuscator.StringDeobfuscating;
+using OpCodes = dnlib.DotNet.Emit.OpCodes;
+using TypeAttributes = dnlib.DotNet.TypeAttributes;
+using MethodAttributes = dnlib.DotNet.MethodAttributes;
+using MethodImplAttributes = dnlib.DotNet.MethodImplAttributes;
+using dnlib.DotNet.Writer;
+using dnlib;
+using OperandType = System.Reflection.Emit.OperandType;
+
+namespace ConfuserDeobfuscator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Test .NET Reactor 6.3.0.0 deobfuscator by Dark_Bull";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Drag and drop file");
+                Thread.Sleep(2000);
+                Environment.Exit(1);
+            }
+            string pathFile = args[0];
+            //string pathFile = "H:\\GitHub\\Madness.NET\\Madness.NET\\Madness.NET\\bin\\x86\\Debug\\MadnessNET_Secure\\MadnessNET.exe";
+            ModuleDef moduleDef = ModuleDefMD.Load(pathFile);
+            Assembly assemblyDef = Assembly.LoadFile(pathFile);
+            MethodInfo methodInfo = (MethodInfo)assemblyDef.EntryPoint;
+            StringDecrypt stringDecrypt = new StringDecrypt(ref moduleDef, ref methodInfo);
+            //var writerOptions = new dnlib.DotNet.Writer.ModuleWriterOptions(moduleDef);
+            //writerOptions.Logger = DummyLogger.NoThrowInstance;
+            moduleDef.Write(Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]) + "_strCleaned" + Path.GetExtension(args[0]));
+            //moduleDef.Write(Path.GetDirectoryName(pathFile) + "\\" + Path.GetFileNameWithoutExtension(pathFile) + "_cleaned" + Path.GetExtension(pathFile));
+            Console.Write("OK!\n");
+
+        }
+    }
+}
---
+++ StringDecrypt/StringDecrypt.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace ConfuserDeobfuscator.StringDeobfuscating
+{
+    public class StringDecrypt
+    {
+        private ModuleDef moduleDef { get; }
+        private MethodInfo methodInfo { get; }
+        public StringDecrypt(ref ModuleDef moduleDef, ref MethodInfo methodInfo)
+        {
+            this.moduleDef = moduleDef;
+            this.methodInfo = methodInfo;
+            Decrypt();
+        }
+
+        void Decrypt()
+        {
+            foreach (var typeDef in moduleDef.Types)
+            {
+                foreach (var methodDef in typeDef.Methods)
+                {
+                    
+                    if (methodDef.Body == null) continue;
+                    for (int i = 0; i < methodDef.Body.Instructions.Count(); i++)
+                    {
+                        if (methodDef.Body.Instructions[i].IsLdcI4() &&
+                            methodDef.Body.Instructions[i + 1].OpCode.Name == "call")
+                        {
+                            dynamic operand_call = methodDef.Body.Instructions[i + 1].Operand;
+                            if (operand_call.ReturnType.TypeName == "String" && operand_call.Parameters[0].Type.TypeName == "Int32")
+                            {
+                                dynamic operand_ldc = methodDef.Body.Instructions[i].Operand;
+                                string decrypt_string = InvokeDecryptMethod(operand_ldc, operand_call.Module.Name, operand_call.DeclaringType2.Name, operand_call.Name.String);
+                                methodDef.Body.Instructions[i].OpCode = OpCodes.Nop;
+                                methodDef.Body.Instructions[i + 1] = new Instruction(OpCodes.Ldstr, decrypt_string);
+                                methodDef.Body.OptimizeBranches();
+                                methodDef.Body.SimplifyBranches();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        string InvokeDecryptMethod(int seed, string moduleName, string typeName, string methodName)
+        {
+            string decryptedString;
+            var module = methodInfo.Module.Assembly.GetModule(moduleName);
+            foreach (var type in module.GetTypes())
+            {
+                if (type.Name == typeName)
+                {
+                    foreach (var methodType in type.GetRuntimeMethods())
+                    {
+                        if (methodType.Name == methodName)
+                        {
+                            decryptedString = (string)methodType.Invoke(null, new object[] { seed });
+                            return decryptedString;
+                        }
+                    }
+                }
+            }
+            return "NULL";
+        }
+    }
+}
